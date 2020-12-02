06bc4d0db224a19a7cc19d6fde61b8107bef6836
hosam mohamed <hosamelmalt@gmail.com>
2020-12-02 12:51:39 -0800

    Initial Commit

---
+++ Food Design/Food_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DesignApp.swift
+//  Food Design
+//
+//  Created by Hossam on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct Food_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
